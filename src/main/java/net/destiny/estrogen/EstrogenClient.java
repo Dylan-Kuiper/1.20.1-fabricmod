e8041f09d61843bc778fb38636423a2c7c79d740
Yodieyogaming <groendyl@gmail.com>
2024-05-23 20:37:14 +0200

    initial commit

---
+++ src/main/java/net/destiny/estrogen/EstrogenClient.java
@@ -0,0 +1,10 @@
+package net.destiny.estrogen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EstrogenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
